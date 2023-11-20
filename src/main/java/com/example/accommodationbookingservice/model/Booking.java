a6a0c079139a9d2af73690f6f023c084907ce3c4
SalnikovMarkiian <salnikov.markiyan@gmail.com>
2023-11-20 19:01:09 +0200

    [model] - added column name & constraints

--- src/main/java/com/example/accommodationbookingservice/model/Booking.java
+++ src/main/java/com/example/accommodationbookingservice/model/Booking.java
@@ -1,6 +1,7 @@
 package com.example.accommodationbookingservice.model;
 
 import java.time.LocalDate;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -10,26 +11,47 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
+@Table(name = "bookings")
+@SQLDelete(sql = "UPDATE orders SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
+@Data
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "check_in", nullable = false)
     private LocalDate checkIn;
+
+    @Column(name = "check_out", nullable = false)
     private LocalDate checkOut;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "accommodation_id", nullable = false)
     private Accommodation accommodation;
 
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
     private Status status;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
 
     public enum Status {
         PENDING, CONFIRMED, CANCELED, EXPIRED
