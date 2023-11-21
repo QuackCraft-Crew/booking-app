083d69fcba646276accacab09656d34522889d29
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-11-21 10:40:16 +0200

    [telegram] basic interaction with chat added

---
+++ src/main/java/com/example/accommodationbookingservice/service/impl/TelegramNotificationServiceImpl.java
@@ -0,0 +1,88 @@
+package com.example.accommodationbookingservice.service.impl;
+
+import com.example.accommodationbookingservice.service.NotificationService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Service
+public class TelegramNotificationServiceImpl extends TelegramLongPollingBot
+        implements NotificationService {
+    @Value("${bot.token}")
+    private String token;
+
+    @Value("${bot.name}")
+    private String name;
+
+    @Value("${bot.chatId}")
+    private String chatId;
+
+    @Override
+    public void sendNotification(String notification) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(notification);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException("Can't send message: " + e);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return name;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            Message message = update.getMessage();
+            String userId = message.getChatId().toString();
+            String firstName = message.getFrom().getFirstName();
+            if (message.getText().equals("/start")) {
+                startCommandResponse(userId, firstName);
+            } else {
+                sendMessage(userId, "This command is currently unavailable!");
+            }
+        }
+    }
+
+    private void startCommandResponse(String chatId, String firstName) {
+        String answer = "Hello, " + firstName + ", nice to meet you!";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(String chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(textToSend);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException("Message was not send: " + e);
+        }
+    }
+
+    @PostConstruct
+    public void init() {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(this);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException("Can't register bot :" + e);
+        }
+    }
+}
