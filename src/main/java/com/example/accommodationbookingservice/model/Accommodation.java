caad1440714b54142f58d5b22c624633ab018d1b
Denis Bohusevich <bogusevich03@gmail.com>
2023-11-21 11:36:00 +0200

    [test] Stripe commit

--- src/main/java/com/example/accommodationbookingservice/model/Accommodation.java
+++ src/main/java/com/example/accommodationbookingservice/model/Accommodation.java
@@ -1,13 +1,38 @@
 package com.example.accommodationbookingservice.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Entity
+@Table(name = "books")
+@Data
 public class Accommodation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Type type;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JoinColumn(name = "address_id", nullable = false)
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    private Address adress;
+    private String size;
+    private String amenities;
+    private BigDecimal dailyRate;
+    private Integer availability;
+    private enum Type {
+        HOUSE,
+        APARTMENT,
+        HOTEL
+    }
 }
