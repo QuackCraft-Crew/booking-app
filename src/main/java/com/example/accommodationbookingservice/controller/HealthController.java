e6752af7d7043b8a51e9f5111c84257d2ab40f74
Denis Bohusevich <bogusevich03@gmail.com>
2023-11-23 22:56:34 +0200

    test

--- src/main/java/com/example/accommodationbookingservice/controller/HealthController.java
+++ src/main/java/com/example/accommodationbookingservice/controller/HealthController.java
@@ -1,12 +1,22 @@
 package com.example.accommodationbookingservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Locale;
 
 @RestController
 public class HealthController {
+    @Autowired
+    private TemplateEngine templateEngine;
     @GetMapping("/health")
     public String healthCheck() {
-        return "OK";
+        String htmlContent = templateEngine.process("test.html",
+                new Context(Locale.getDefault()));
+        return htmlContent;
     }
 }
