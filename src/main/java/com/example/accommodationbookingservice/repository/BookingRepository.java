4f1e84ae74d4502d90747e5d9fd870c22fcd233d
Denis Bohusevich <bogusevich03@gmail.com>
2023-11-23 12:23:04 +0200

    [booking] implement cancel status when deleting booking second edition ,codestyle fix

--- src/main/java/com/example/accommodationbookingservice/repository/BookingRepository.java
+++ src/main/java/com/example/accommodationbookingservice/repository/BookingRepository.java
@@ -3,12 +3,8 @@ package com.example.accommodationbookingservice.repository;
 import static com.example.accommodationbookingservice.model.Booking.Status;
 
 import com.example.accommodationbookingservice.model.Booking;
-import jakarta.transaction.Transactional;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
@@ -17,6 +13,5 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByUserId(Long userId);
 
     List<Booking> findByStatus(Status status);
-
 
 }
