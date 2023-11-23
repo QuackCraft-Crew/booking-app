3f5dd366d019f77cbff7b2591969acde5ad48faa
Roman Yuzviak <3romanyuzviak@gmail.com>
2023-11-23 22:33:36 +0200

    splitted admin and user enpoinds for GET payment

--- src/main/java/com/example/accommodationbookingservice/controller/PaymentController.java
+++ src/main/java/com/example/accommodationbookingservice/controller/PaymentController.java
@@ -3,15 +3,20 @@ package com.example.accommodationbookingservice.controller;
 import com.example.accommodationbookingservice.dto.payment.CreatePayment;
 import com.example.accommodationbookingservice.dto.payment.CreatePaymentResponseDto;
 import com.example.accommodationbookingservice.dto.payment.PaymentResponseDto;
+import com.example.accommodationbookingservice.exception.EntityNotFoundException;
+import com.example.accommodationbookingservice.model.User;
+import com.example.accommodationbookingservice.repository.UserRepository;
 import com.example.accommodationbookingservice.service.PaymentService;
 import com.example.accommodationbookingservice.service.impl.StripeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.security.Principal;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,9 +34,20 @@ public class PaymentController {
     private StripeService stripeService;
     @Autowired
     private PaymentService paymentService;
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @Operation(summary = "Get payments for logged in user",
+            description = "Get all payments by user id")
+    public List<PaymentResponseDto> getPaymentsByUserId(Principal principal) {
+        return paymentService.getPaymentsByUserId(getUserId(principal));
+    }
 
     @GetMapping("/{userId}")
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     @Operation(summary = "Get payments by user id",
             description = "Get all payments by user id")
     public List<PaymentResponseDto> getPaymentsByUserId(@PathVariable Long userId) {
@@ -61,5 +77,11 @@ public class PaymentController {
     public String getCancelResponse(@RequestParam("session_id") String sessionId) {
         paymentService.cancelPayment(sessionId);
         return "cancel payment with session id: " + sessionId;
+    }
+
+    private Long getUserId(Principal principal) {
+        User user = userRepository.findByEmail(principal.getName())
+                .orElseThrow(() -> new EntityNotFoundException("User is not found"));
+        return user.getId();
     }
 }
