de46352b5ec0cd835de9f127de3083a29d66a710
Bogdan <97698639+don-bigdad@users.noreply.github.com>
2023-11-23 22:38:55 +0200

    [payment] - mvn fix

--- src/main/java/com/example/accommodationbookingservice/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/example/accommodationbookingservice/service/impl/PaymentServiceImpl.java
@@ -14,7 +14,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
